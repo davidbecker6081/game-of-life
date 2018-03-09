e39d55eec3700d9bcf325e36b7c1676404bdf337
David Becker <david@beckersite.com>
2018-03-09 12:29:11 -0700

    Add method for initial drawing of map

--- game.js
+++ game.js
@@ -42,4 +42,13 @@ function initializeMap() {
   console.log(mapOfCells);
 }
 
+function drawMap() {
+  for (let i = 0; i < xAxisCells; i++) {
+    for (let j = 0; j < yAxisCells; j++) {
+      mapOfCells[i][j].draw();
+    }
+  }
+}
+
 initializeMap();
+drawMap();
