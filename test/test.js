f1b09861db6d095605732ec5c99a7b10ac7b1372
David Becker <david@beckersite.com>
2018-03-20 18:35:05 -0600

    Initial setup for testing

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const assert = chai.assert;
+const should = chai.should();
+
+describe('test', () => {
+  it('should run a test', () => {
+    assert.equal('a', 'a')
+  })
+})
