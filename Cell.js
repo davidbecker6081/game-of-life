fd17f2ded20c5682070165b8b5f9eb606fa3a578
David Becker <david@beckersite.com>
2018-03-21 18:36:35 -0600

    Refactor Cell Class into separate file

---
+++ Cell.js
@@ -0,0 +1,54 @@
+module.exports = class Cell {
+  constructor() {
+    this.x = x;
+    this.y = y;
+    this.size = cellSize;
+    this.isAlive = false;
+    this.neighbors = 0;
+  }
+
+  getNeighbors = (mapOfCells) => {
+    if (this.x > 0 && mapOfCells[this.x - 1][this.y].isAlive) {
+      this.neighbors++;
+    }
+    if (this.x < xAxisCells - 1 && mapOfCells[this.x + 1][this.y].isAlive) {
+      this.neighbors++;
+    }
+    if (this.y < yAxisCells - 1 && mapOfCells[this.x][this.y + 1].isAlive) {
+      this.neighbors++;
+    }
+    if (this.y > 0 && mapOfCells[this.x][this.y - 1].isAlive) {
+      this.neighbors++;
+    }
+    if (this.x > 0 && this.y > 0 && mapOfCells[this.x - 1][this.y - 1].isAlive) {
+      this.neighbors++;
+    }
+    if (this.x < xAxisCells - 1 && this.y > 0 && mapOfCells[this.x + 1][this.y - 1].isAlive) {
+      this.neighbors++;
+    }
+    if (this.y < yAxisCells - 1 && this.x > 0 && mapOfCells[this.x - 1][this.y + 1].isAlive) {
+      this.neighbors++;
+    }
+    if (this.x < xAxisCells - 1 && this.y < yAxisCells - 1 && mapOfCells[this.x + 1][this.y + 1].isAlive) {
+      this.neighbors++;
+    }
+  }
+
+  draw = (ctx) => {
+    ctx.beginPath();
+    ctx.rect(this.x * this.size, this.y * this.size, this.size, this.size);
+    ctx.strokeStyle = '#c0c3c5';
+    ctx.stroke();
+    ctx.closePath();
+
+    if (this.isAlive) {
+      ctx.fillStyle = '#50C878';
+    } else {
+      ctx.fillStyle = '#f3f3f3';
+    }
+    ctx.fillRect(this.x * this.size, this.y * this.size, this.size, this.size);
+    ctx.stroke();
+
+    return ctx
+  }
+}
